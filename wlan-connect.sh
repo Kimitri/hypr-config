155490c42b2ebe0c89ea7654708df409a18d276e
Kimmo Tapala <kimmo.tapala@karhuhelsinki.fi>
2023-12-04 20:50:07 +0200

    Add hyprland notifications to WIFI connection script

--- wlan-connect.sh
+++ wlan-connect.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 # Connect to a wireless network using nmcli
 
+notify () {
+    INSTANCE=$(hyprctl instances | awk 'NR==1{print $2}' | sed 's/.$//')
+    hyprctl -i $INSTANCE notify -1 5000 "rgb(ff1ea3)" "$1"
+}
+
 WLAN_SSID="VMKG"
+
+notify "Connecting to ${WLAN_SSID}..."
 
 SSID_FOUND=$(nmcli device wifi list | grep ${WLAN_SSID})
 
 if [ "${SSID_FOUND}" ]; then
-    echo "Network ${WLAN_SSID} found!"
+    notify "Network ${WLAN_SSID} found!"
     nmcli connection up ${WLAN_SSID} passwd-file /home/kimmo/.config/hypr/nmconnection-vmkg.passwd
 else
-    echo "Network ${WLAN_SSID} not found!"
+    notify "Network ${WLAN_SSID} not found!"
 fi
